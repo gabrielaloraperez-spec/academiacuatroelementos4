c4aae660d52951beb673f9fe14cf4f85453ea8f3
gabrielaloraperez-spec <gabrielaloraperez@gmail.com>
2026-03-01 14:42:18 -0500

    docs: add vercel 404 project-not-found recovery steps

--- academia-cuatro-elementos/academia-cuatro-reinos/src/screens/index.ts
+++
@@ -1,7 +0,0 @@
-export { WelcomeScreen } from './WelcomeScreen';
-export { MapScreen } from './MapScreen';
-export { LevelScreen } from './LevelScreen';
-export { KnowledgeRoom } from './KnowledgeRoom';
-export { BossScreen } from './BossScreen';
-export { GameOverScreen } from './GameOverScreen';
-export { DomainChallengeScreen } from './DomainChallengeScreen';
